698cd8ff90ff5983c9e454264f69a4011034cd96
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-07-06 18:30:35 +0100

    One more function completed

--- fizzbuzz_interview_question.py
+++ fizzbuzz_interview_question.py
@@ -1,16 +1,16 @@
 def fizz_buzz(num):
-    """Takes a number as an argument and returns 'Fizz', 'Buzz' or 'FizzBuzz'
+	"""Takes a number as an argument and returns 'Fizz', 'Buzz' or 'FizzBuzz'
 
    Instructions:
 
-    If the number is a multiple of 3 the output should be "Fizz".
-    If the number given is a multiple of 5, the output should be "Buzz".
-    If the number given is a multiple of both 3 and 5, the output should be "FizzBuzz".
-    If the number is not a multiple of either 3 or 5, the number should be output on its own as shown in the examples below.
-    The output should always be a string even if it is not a multiple of 3 or 5.
+	If the number is a multiple of 3 the output should be "Fizz".
+	If the number given is a multiple of 5, the output should be "Buzz".
+	If the number given is a multiple of both 3 and 5, the output should be "FizzBuzz".
+	If the number is not a multiple of either 3 or 5, the number should be output on its own as shown in the examples below.
+	The output should always be a string even if it is not a multiple of 3 or 5.
 
-    :param num: int
-    :return: string
-    """
+	:param num: int
+	:return: string
+	"""
 
 	return "Fizz"*(num%3==0) + "Buzz"*(num%5==0) or str(num)
