0065978b524c8d0d3c2ea05993e495823797d54d
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-07-07 15:15:03 +0100

    Halve and halve again

--- halve_count().py
+++ halve_count().py
@@ -1,7 +1,10 @@
-def halve_count(a,b):
+def halve_count(a, b):
     """Given two integers a and b, return how many times a can be halved while still being greater than b"""
     i = 0
     while a > b:
-        a = a/2
+        a = a / 2
         i += 1
-    return i-1
+    return i - 1
+
+
+print(halve_count(15, 5))
