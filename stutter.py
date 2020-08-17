a7997c5464b2c8490577dcd1d84dd2f5fe3d5021
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-08-17 12:20:07 +0200

    Sttutering Function

---
+++ stutter.py
@@ -0,0 +1,10 @@
+def stutter(word):
+    """ Write a function that stutters a word as if someone is struggling to read it.
+
+    :param word: str
+    :return: str
+    """
+    return str(2 * (word[:2] + '... ') + word + '?')
+
+
+print(stutter('incredible'))
