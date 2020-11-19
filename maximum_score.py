c3e291a0684846bc880285b738f8c98dd96a7bdc
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-11-19 22:37:59 +0100

    Sum score from dict key

---
+++ maximum_score.py
@@ -0,0 +1,16 @@
+def maximum_score(tile_hand):
+    """Return highest score in hand"""
+    return sum([i['score'] for i in tile_hand])
+
+
+print(maximum_score([
+  { "tile": "N", "score": 1 },
+  { "tile": "K", "score": 5 },
+  { "tile": "Z", "score": 10 },
+  { "tile": "X", "score": 8 },
+  { "tile": "D", "score": 2 },
+  { "tile": "A", "score": 1 },
+  { "tile": "E", "score": 1 }
+]))
+
+
