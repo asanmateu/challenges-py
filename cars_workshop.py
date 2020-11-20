7dae95a23f7c3b3e194994eee7535b2e4b20b847
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-11-20 15:39:14 +0100

    Minimum full toys (floor div)

--- cars_workshop.py
+++ cars_workshop.py
@@ -2,4 +2,5 @@ def cars(wheels, bodies, figures):
     """Return number of complete toy cars that can be made"""
     return min(wheels // 4, bodies, figures // 2)
 
-print(cars(6,3,25))
+
+print(cars(6, 3, 25))
