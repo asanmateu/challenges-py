fdfd6740fe43319a20282dfe1062f7f67638bc7e
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-11-20 16:17:52 +0100

    Replace vowels with character

--- replace_vowels.py
+++ replace_vowels.py
@@ -13,3 +13,17 @@ print(replace_vowels("the aardvark", "#"))
 print(replace_vowels("minnie mouse", "?"))
 print(replace_vowels("shakespeare", "*"))
 
+# Alternative pathways:
+# Option 1:
+
+import re
+
+
+def replace_vowels(txt, ch):
+    return re.sub(r'[aeiouAEIOU]', ch, txt)
+
+
+# Option 2:
+
+def replace_vowels(txt, ch):
+    return ''.join([i if i not in 'aeoui' else ch for i in txt])
