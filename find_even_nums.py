ba8ebff6b235c16498df9e011d2ec6f9bb58c8af
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-11-25 23:25:46 +0100

    Find even numbers in range

---
+++ find_even_nums.py
@@ -0,0 +1,8 @@
+def find_even_nums(num):
+    """ Return all even numbers above 0 up to num
+    :param num: int
+    :return: list of ints
+    """
+    return [i for i in range(num+1) if i % 2 == 0 and i != 0]
+
+print(find_even_nums(10))
