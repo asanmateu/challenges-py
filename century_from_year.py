8fd48a89a2350953338e2d2da2106b3920458b81
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-11-26 00:22:48 +0100

    Return century from year

---
+++ century_from_year.py
@@ -0,0 +1,12 @@
+import math
+
+def century_from_year(year):
+    """ Return century from year
+    :param year: int
+    :return: int
+    """
+    return math.ceil(year/100)
+
+
+print(century_from_year(1982))
+print(century_from_year(2001))
