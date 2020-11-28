8992785cd457f861369f66a4959721d74b7a7f35
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-11-28 17:04:17 +0100

    Dict to list of tuples alphabetically

---
+++ dict_to_tuplelist.py
@@ -0,0 +1,13 @@
+def dict_to_list(d):
+    """ Convert dictionary to list of tuples in alphabetical order
+    :param d: dict
+    :return: list of tuples
+    """
+    return sorted([(k, v) for k, v in d.items()])
+
+
+d1 = {'D': 1, 'B': 2, 'C': 3}
+d2 = {'likes': 2, 'dislikes': 3, 'followers': 10}
+
+print(dict_to_list(d1))
+print(dict_to_list(d2))
